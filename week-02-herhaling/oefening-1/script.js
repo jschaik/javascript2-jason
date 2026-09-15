d94d81dc8b4b42e5368514095fc90d404bda294e
Jason <j.vanschaik@tcrmbo.nl>
2026-09-15 10:39:15 +0200

    opdracht 1 afgerond

--- week-02-herhaling/oefening-1/script.js
+++ week-02-herhaling/oefening-1/script.js
@@ -7,32 +7,40 @@
 // 1. Schrijf een functie die twee getallen optelt en het resultaat teruggeeft
 function optellen(a, b) {
   // TODO
+ return a + b
 }
+
 
 // 2. Schrijf een functie die true teruggeeft als het getal even is, anders false
 function isEven(getal) {
   // TODO
+  // javascript modulo
+  return getal % 2 === 0;
 }
+
 
 // 3. Schrijf een functie die een begroeting teruggeeft: 'Hallo, <naam>!'
 function groet(naam) {
   // TODO
+  return "Hallo, " + naam ;
 }
 
 // 4. Schrijf een functie die celsius omrekent naar fahrenheit
 //    formule: celsius * 9 / 5 + 32
 function celsiusNaarFahrenheit(celsius) {
   // TODO
+   return (celsius * 9) / 5 + 32
+
 }
 
 // Dit hoef je niet aan te passen — dit toont je resultaten in de DOM:
 document.getElementById('result-optellen').textContent =
-  '3 + 5 = ' + optellen(3, 5);
+  '3 + 5 = ' + optellen(9, 180);
 
 document.getElementById('result-even').textContent =
   'Is 8 even? ' + isEven(8);
 
-document.getElementById('result-groet').textContent = groet('Sanne');
+document.getElementById('result-groet').textContent = groet('Sanna');
 
 document.getElementById('result-temp').textContent =
   '20°C is ' + celsiusNaarFahrenheit(20) + '°F';
